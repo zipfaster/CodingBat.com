81e48864627b406904ab6948fb095594fdc159bf
zipfaster <99938603+zipfaster@users.noreply.github.com>
2023-12-13 18:34:37 -0800

    Create backAround.java

---
+++ Java/Warmup-1/backAround.java
@@ -0,0 +1,28 @@
+/*
+backAround.java
+https://codingbat.com/prob/p161642
+
+Given a string, take the last char and 
+return a new string with the last char added at the front and back, 
+so "cat" yields "tcatt". The original string will be length 1 or more.
+
+Output:
+backAround("cat") → "tcatt"
+backAround("Hello") → "oHelloo"
+backAround("a") → "aaa"
+*/
+
+public String backAround(String str) {
+  char end = str.charAt(str.length() -1);
+  return end + str + end;
+}
+
+/*
+All Correct:
+backAround("cat") → "tcatt"
+backAround("Hello") → "oHelloo"
+backAround("a") → "aaa"
+backAround("abc") → "cabcc"
+backAround("read") → "dreadd"
+backAround("boo") → "obooo"
+*/
