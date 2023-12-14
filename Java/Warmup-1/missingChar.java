8d306f6bb6eda07d57593bc76ff376d48d4912a3
zipfaster <99938603+zipfaster@users.noreply.github.com>
2023-12-13 18:21:31 -0800

    Create missingChar.java

---
+++ Java/Warmup-1/missingChar.java
@@ -0,0 +1,32 @@
+/*
+missingChar.java
+https://codingbat.com/prob/p190570
+
+Given a non-empty string and an int n, 
+return a new string where the char at index n has been removed. 
+The value of n will be a valid index of a char in the original string 
+(i.e. n will be in the range 0..str.length()-1 inclusive).
+
+Output:
+missingChar("kitten", 1) → "ktten"
+missingChar("kitten", 0) → "itten"
+missingChar("kitten", 4) → "kittn"
+*/
+
+public String missingChar(String str, int n) {
+  return (str.substring(0,n)) + (str.substring(n+1, str.length()));
+}
+
+/*
+All Correct:
+missingChar("kitten", 1) → "ktten"
+missingChar("kitten", 0) → "itten"
+missingChar("kitten", 4) → "kittn"
+missingChar("Hi", 0) → "i"
+missingChar("Hi", 1) → "H"
+missingChar("code", 0) → "ode"
+missingChar("code", 1) → "cde"
+missingChar("code", 2) → "coe"
+missingChar("code", 3) → "cod"
+missingChar("chocolate", 8) → "chocolat"
+*/
