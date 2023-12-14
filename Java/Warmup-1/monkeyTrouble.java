a091d2759c8b79cd07b8f4a519a52ebdbfa10cf5
zipfaster <99938603+zipfaster@users.noreply.github.com>
2023-12-13 16:10:06 -0800

    Create monkeyTrouble.java

---
+++ Java/Warmup-1/monkeyTrouble.java
@@ -0,0 +1,24 @@
+/*
+monkeyTrouble.java
+https://codingbat.com/prob/p181646
+
+We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. 
+We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+
+Output:
+monkeyTrouble(true, true) → true
+monkeyTrouble(false, false) → true
+monkeyTrouble(true, false) → false
+*/
+
+public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+  return (aSmile && bSmile) || (!aSmile && !bSmile);
+}
+
+/*
+All Correct:
+monkeyTrouble(true, true) → true	
+monkeyTrouble(false, false) → true
+monkeyTrouble(true, false) → false	
+monkeyTrouble(false, true) → false
+*/
